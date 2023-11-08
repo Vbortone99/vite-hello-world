53cfd95f7264cb82e2d5ac898f4e8e1dae049478
Vbortone99 <bortonevincenzo99@gmail.com>
2023-11-08 21:44:57 +0100

    AppTitle component create

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <header>
+    <h1></h1>
+  </header>
+</template>
+
+<script>
+  export default {
+    components:{
+
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  h1{
+    text-align: center;
+    color: var(color darkgreen);
+  }
+</style>
